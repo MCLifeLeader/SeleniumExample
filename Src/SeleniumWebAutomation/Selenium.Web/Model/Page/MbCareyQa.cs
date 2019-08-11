b86108904800545d7f21eb64451814c3fd6df212
Michael Carey <Michael@AGameEmpowerment.com>
2019-08-10 21:26:40 -0600

    Adding two page models to the test example.

---
+++ Src/SeleniumWebAutomation/Selenium.Web/Model/Page/MbCareyQa.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.Web.Model.Page
+{
+   public class MbCareyQa : WebDriverConfig
+   {
+      public static IWebElement MainPageText => Driver.FindElement(By.XPath("/html/body/div[1]/partial/main/h2"));
+   }
+}
---
+++ Src/SeleniumWebAutomation/Selenium.Web/Model/Page/MbCareyTechnologies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Selenium.Web.Model.Page
+{
+   public class MbCareyTechnologies : WebDriverConfig
+   {
+      public static IWebElement MainPageText => Driver.FindElement(By.XPath("/html/body/div[1]/partial/main/h2"));
+      public static ReadOnlyCollection<IWebElement> HyperLinkList => Driver.FindElements(By.TagName("a"));
+      public static IWebElement QualityAssuranceLink => Driver.FindElement(By.XPath("/html/body/div[1]/partial/main/div/div[7]/p[2]/a"));
+   }
+}
