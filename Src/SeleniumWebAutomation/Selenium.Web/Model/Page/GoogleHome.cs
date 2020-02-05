73eb188b81586fe49b3564cf8a039b385708387e
Michael Carey <Michael@AGameEmpowerment.com>
2020-02-04 20:53:21 -0700

    Updated xpath for google.com homepage.

--- Src/SeleniumWebAutomation/Selenium.Web/Model/Page/GoogleHome.cs
+++ Src/SeleniumWebAutomation/Selenium.Web/Model/Page/GoogleHome.cs
@@ -8,6 +8,6 @@ namespace Selenium.Web.Model.Page
 
       public static IWebElement SearchForm => Driver.FindElement(By.XPath("/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input"));
 
-      public static IWebElement GoogleSearchBtn => Driver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[3]/center/input[1]"));
+      public static IWebElement GoogleSearchBtn => Driver.FindElement(By.XPath("/html/body/div/div[3]/form/div[2]/div[1]/div[3]/center/input[1]"));
    }
 }
