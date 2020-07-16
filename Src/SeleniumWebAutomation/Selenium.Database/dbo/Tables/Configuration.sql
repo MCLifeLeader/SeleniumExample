035bcc6d02bda4ff1e1fc7f5bc6636e4c82f0091
Michael Carey <Michael@AGameEmpowerment.com>
2020-07-16 17:22:47 -0600

    Minor file formatting changes.

--- Src/SeleniumWebAutomation/Selenium.Database/dbo/Tables/Configuration.sql
+++ Src/SeleniumWebAutomation/Selenium.Database/dbo/Tables/Configuration.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Configuration] (
-    [Id]       UNIQUEIDENTIFIER     NOT NULL DEFAULT NewId(),
-    [Key]      NVARCHAR(50)         NOT NULL,
-    [Value]    NVARCHAR(MAX)        NOT NULL,
-    [Note]     NVARCHAR(MAX)        NULL,
-    [IsActive] BIT                  NOT NULL DEFAULT 1, 
-    [DateCreated] DATETIME NOT NULL DEFAULT GetUtcDate(), 
-    [LastUpdated] DATETIME NOT NULL DEFAULT GetUtcDate(), 
+    [Id]          UNIQUEIDENTIFIER     NOT NULL DEFAULT NewId(),
+    [Key]         NVARCHAR(50)         NOT NULL,
+    [Value]       NVARCHAR(MAX)        NOT NULL,
+    [Note]        NVARCHAR(MAX)        NULL,
+    [IsActive]    BIT                  NOT NULL DEFAULT 1, 
+    [DateCreated] DATETIME             NOT NULL DEFAULT GetUtcDate(), 
+    [LastUpdated] DATETIME             NOT NULL DEFAULT GetUtcDate(), 
     CONSTRAINT [PK_Configuration] PRIMARY KEY ([Id])
 );
 
