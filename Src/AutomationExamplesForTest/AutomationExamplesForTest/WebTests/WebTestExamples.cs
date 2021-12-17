e2162fc72735bac292cd4ff99f2df6e21f8ce388
Michael Carey <Michael@AGameEmpowerment.com>
2021-12-17 12:13:06 -0700

    Setting up updated Automation Example Project in .NET 6

---
+++ Src/AutomationExamplesForTest/AutomationExamplesForTest/WebTests/WebTestExamples.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace AutomationExamplesForTest.WebTests
+{
+    [TestFixture]
+    public class WebTestExamples
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
