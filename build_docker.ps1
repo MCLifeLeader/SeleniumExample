fe43037e8039245ab9f9c5dff33cfb597711fd60
Michael Carey <Michael@AGameEmpowerment.com>
2022-01-13 14:11:29 -0700

    Updated the build to reference the nuget.config file.

--- build_docker.ps1
+++ build_docker.ps1
@@ -1,11 +1,11 @@
 # Build the AutomationExample.WebApi project
-Set-Location .\Src\Docker\AutomationExample.WebApi\
-dotnet build --configuration Release --source .\AutomationExample.WebApi.sln
+Set-Location .\Src\Docker\AutomationExample.WebApi\ 
+dotnet build --configuration Release --source .\AutomationExample.WebApi.sln --source ..\..\..\Nuget\nuget.config
 docker build -f ".\AutomationExample.WebApi\Dockerfile" --force-rm -t automationexamplewebapi  --label "com.microsoft.created-by=visual-studio" --label "com.microsoft.visual-studio.project-name=AutomationExample.WebApi" ".\"
 
 # Build the AutomationExample.WebApp project
 Set-Location ..\AutomationExample.WebApp\
-dotnet build --configuration Release --source .\AutomationExample.WebApp.sln
+dotnet build --configuration Release --source .\AutomationExample.WebApp.sln --source ..\..\..\Nuget\nuget.config
 docker build -f ".\AutomationExample.WebApp\Dockerfile" --force-rm -t automationexamplewebapp  --label "com.microsoft.created-by=visual-studio" --label "com.microsoft.visual-studio.project-name=AutomationExample.WebApp" ".\"
 
 Set-Location ..\..\..
--- run_tests.ps1
+++ run_tests.ps1
@@ -1,6 +1,6 @@
 # Run the automated tests
 Set-Location .\Src\Tests\AutomationExample.Tests\
-dotnet build --configuration Debug --source .\AutomationExample.Tests.sln
+dotnet build --configuration Debug --source .\AutomationExample.Tests.sln --source ..\..\..\Nuget\nuget.config
 dotnet test .\AutomationExample.Tests.sln
 
 Set-Location ..\..\..
