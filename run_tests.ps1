a953aeb7df05889197f914f2280ed82d2d069e7f
Michael Carey <Michael@AGameEmpowerment.com>
2022-01-15 10:41:00 -0700

    Added commandline argument to log the test results to a file.

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,6 +1,9 @@
+# Setup dotnet test run argument
+[string] $loggerArgument = '\"trx;logfilename=Test_Results.' + (Get-Date).ToString("yyyyMMdd.HHmmss") + '.trx\"'
+
 # Run the automated tests
 Set-Location .\Src\Tests\AutomationExample.Tests\
 dotnet build --configuration Debug --source .\AutomationExample.Tests.sln --source ..\..\..\Nuget\nuget.config
-dotnet test .\AutomationExample.Tests.sln
+dotnet test .\AutomationExample.Tests.sln --logger $loggerArgument #--collect:"Code Coverage"
 
 Set-Location ..\..\..
